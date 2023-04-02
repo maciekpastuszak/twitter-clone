1cd17874b5910f7dab8c9a58d2e672c4b8fcb06c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-02 11:26:23 +0200

    add wrappers around children in Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,8 +4,18 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
   return (
-    <div>
-        {children}
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <div className="
+          col-span-3
+          lg:col-span-2border-x-[1px]
+          border-neutral-800
+          ">
+          {children}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
