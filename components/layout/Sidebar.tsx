fd893c982d8f188e6c456b9da2c830ddfef8b8fe
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-02 11:45:14 +0200

    add SidebarLogo component

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
+import SidebarLogo from './SidebarLogo';
 
 const Sidebar = () => {
     const items = [
@@ -23,7 +24,7 @@ const Sidebar = () => {
         <div className="col-span-1 h-full pr-4 md:pr-6">
             <div className="flex flex-col items-end">
                 <div className="space-y-2 lg:w-[230px]">
-
+                    <SidebarLogo />
                 </div>
             </div>
         </div>
---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,7 @@
+const SidebarLogo = () => {
+    return ( 
+        <div></div>
+     );
+}
+
+export default SidebarLogo;
