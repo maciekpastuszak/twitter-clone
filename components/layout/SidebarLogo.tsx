d81b62dc71ea7d4ee60ce96dd3cfddc76a3b7dfc
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-02 12:04:23 +0200

    add styles to SidebarLogo and add twitter icon

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -1,6 +1,24 @@
+import { useRouter } from "next/router";
+import { BsTwitter } from "react-icons/bs";
+
 const SidebarLogo = () => {
+    const router = useRouter();
     return ( 
-        <div></div>
+        <div className="
+            rounded-full
+            h-14
+            w-14
+            p-4
+            flex
+            items-center
+            justify-center
+            hover:bg-blue-300
+            hover:bg-opacity-10
+            cursor-pointer
+            transition
+        ">
+            <BsTwitter size={28} color="white"/>
+        </div>
      );
 }
 
