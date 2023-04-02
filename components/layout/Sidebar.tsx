081095591400e232c9fe7c1f2ad30e590866c878
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-02 11:42:50 +0200

    add wrapper around Sidebar logo

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -20,8 +20,12 @@ const Sidebar = () => {
         }
     ]
     return ( 
-        <div>
+        <div className="col-span-1 h-full pr-4 md:pr-6">
+            <div className="flex flex-col items-end">
+                <div className="space-y-2 lg:w-[230px]">
 
+                </div>
+            </div>
         </div>
      )
 }
