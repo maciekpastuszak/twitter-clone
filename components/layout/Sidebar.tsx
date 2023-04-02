778a4763cfde42ea78644ac4b66b71e91939ae81
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-02 11:41:00 +0200

    add items to Sidebar component

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,6 +1,28 @@
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
 const Sidebar = () => {
+    const items = [
+        {
+            label: 'Home',
+            href: '/',
+            icon: BsHouseFill
+        },
+        {
+            label: 'Notifications',
+            href: '/notifications',
+            icon: BsBellFill
+        },
+        {
+            label: 'Profile',
+            href: '/users/123',
+            icon: FaUser
+        }
+    ]
     return ( 
-        <div></div>
+        <div>
+
+        </div>
      )
 }
 
