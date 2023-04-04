228ed1fde76cad08455c16fde5ea175abaff2ab5
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-04 23:13:40 +0200

    add large screen twitter button

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -23,6 +23,29 @@ const SidebarTweetButton = () => {
             >
                 <FaFeather size={24} color="white" />
             </div>
+            <div className="
+                mt-6
+                hidden
+                lg:block
+                px-4
+                py-2
+                rounded-full
+                bg-sky-500
+                hover:bg-opacity-90
+                cursor-pointer
+                transition
+            ">
+                <p className="
+                    hidden
+                    lg:block
+                    text-center
+                    font-semibold
+                    text-white
+                    text-[20px]
+                ">
+                    Tweet
+                </p>
+            </div>
         </div>
     );
 }
