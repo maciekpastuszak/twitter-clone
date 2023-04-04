dc98bdd7dde372deb3d0c5813f7cec17e56a26a4
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-04 22:30:04 +0200

    develop SidebarItem component

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,7 +1,22 @@
-const SidebarItem = () => {
+import { IconType } from 'react-icons';
+
+
+interface SidebarItemProps {
+    label: string;
+    href?: string;
+    icon: IconType;
+    onClick?: () => void
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    label,
+    href,
+    icon,
+    onClick
+}) => {
     return (
         <div>
-            
+
         </div>
     )
 }
