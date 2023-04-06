4720300f2d319a1fe2471c5b9d09f56b52a00789
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-06 22:46:17 +0200

    add header to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-   <div className="text-3xl text-sky-500">
-    Hello World!
-   </div>
+    <>
+    <Header label="Home" />
+    </>
   )
 }
