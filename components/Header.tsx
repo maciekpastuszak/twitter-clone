49aaf5bdd25b30976f99b258dbe95b2343b4e2b4
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-06 22:55:58 +0200

    add styling to Header wrapper

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useCallback } from 'react';
+import { BiArrowBack } from 'react-icons/bi';
 
 interface HeaderProps {
     label: string;
@@ -14,7 +15,15 @@ const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
     }, [router]);
 
     return ( 
-        <div></div>
+        <div className="border-b-[1px] border-neutral-800 p-5">
+            <div className="flex flex-row items-center gap-2">
+                {
+                    showBackArrow && (
+                        <BiArrowBack />
+                    )
+                }
+            </div>
+        </div>
     );
 }
 
