3990a356d042b4be186d79d689a303157f098859
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-06 22:52:18 +0200

    add router and props interface to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,21 @@
-const Header = () => {
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+
+interface HeaderProps {
+    label: string;
+    showBackArrow?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
+    const router = useRouter();
+
+    const handleBack = useCallback(() => {
+        router.back();
+    }, [router]);
+
     return ( 
         <div></div>
-    )
+    );
 }
 
 export default Header;
