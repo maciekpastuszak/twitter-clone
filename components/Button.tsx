788e2c2456305130cb180fc2335bc67aaa498d64
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-06 23:14:18 +0200

    add styles for props in Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -19,14 +19,24 @@ const Button: React.FC<ButtonProps> = ({
     outline
 }) => {
     return (
-        <button className="
+        <button className={`
             diasbled:opacity-70
             disabled:cursor-not-allowed
             rounded-full
             font-semibold
             hover:opacity-80transition
             border-2
-        ">
+            ${fullWidth ? 'w-full' : 'w-fit'}
+            ${secondary ? 'bg-white' : 'bg-sky-500'}
+            ${secondary ? 'text-black' : 'text-white'}
+            ${secondary ? 'border-black' : 'border-sky-500'}
+            ${large ? 'text-xl' : 'text-md'}
+            ${large ? 'px-5' : 'px-4'}
+            ${large ? 'py-3' : 'py-2'}
+            ${outline ? 'bg-transapretnt' : ''}
+            ${outline ? 'border-white' : ''}
+            ${outline ? 'text-white' : ''}
+            `}>
 
         </button>
     )
