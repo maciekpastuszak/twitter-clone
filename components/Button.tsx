f0e79602d54bf01d9e9c0edd1b4a9e879f7b8004
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-06 23:05:15 +0200

    add Button component

---
+++ components/Button.tsx
@@ -0,0 +1,18 @@
+
+interface ButtonProps {
+    label: string;
+    secondary?: boolean;
+    fullWidth?: boolean;
+    large?: boolean;
+    onClick: () => void;
+    disabled?: boolean;
+    outline?: boolean
+}
+
+const Button = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Button;
