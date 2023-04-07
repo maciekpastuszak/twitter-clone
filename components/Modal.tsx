d9af4d7da5566f0ddb91e0f2c6b975e6a5d56524
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-07 09:07:37 +0200

    add props interface to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,3 +1,14 @@
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+}
+
 
 const Modal = () => {
     return(
