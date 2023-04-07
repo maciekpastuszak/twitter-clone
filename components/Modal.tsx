1440d1ebdaade54333f0dd2d0477705a94a641e7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-07 09:30:13 +0200

    add testing modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -72,22 +72,36 @@ const Modal: React.FC<ModalProps> = ({
            "
            >
             {/* Content */}
-            <div className="
-            h-full
-            lg:h-autoborder-0
-            rounded-lg
-            shadow-lg
-            relative
-            flex
-            flex-col
-            w-full
-            bg-black
-            outline-none
-            focus:outline-none
-            "
-            >
-
-            </div>
+                <div className="
+                h-full
+                lg:h-autoborder-0
+                rounded-lg
+                shadow-lg
+                relative
+                flex
+                flex-col
+                w-full
+                bg-black
+                outline-none
+                focus:outline-none
+                "
+                >
+                    {/* Header */}
+                    <div className="
+                        flex
+                        items-center
+                        justify-between
+                        p-10
+                        rounded-t
+                    ">
+                        <h3 className="
+                            text-3xl
+                            font-semibold
+                            text-white
+                        "
+                        >{title}</h3>
+                    </div>
+                </div>
             </div> 
         </div>
        </>
