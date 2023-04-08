3917ffa622c3244b5db0af54599c1e6073c9447a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-08 19:12:33 +0200

    build input props and styling

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,9 +6,38 @@ interface InputProps {
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = () => {
+const Input: React.FC<InputProps> = ({
+    placeholder,
+    value,
+    type,
+    disabled,
+    onChange
+}) => {
     return (
-        <div></div>
+        <input
+            disabled={disabled}
+            onChange={onChange}
+            value={value}
+            placeholder={placeholder}
+            type={type}
+            className="
+                w-full
+                p-4
+                text-lg
+                bg-black
+                border-2
+                border-neutral-800
+                rounded-md
+                outline-none
+                text-white
+                focus:border-sky-500
+                focus:border-2
+                transation
+                disabled:bg-neutral-900
+                disabled:opacity-70
+                disabled:cursor-not-allowed
+            "
+        />
     );
 }
 
