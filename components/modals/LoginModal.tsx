3b772d38f12872d3e81b6e5ee4e20dba800ca92c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-08 19:29:55 +0200

    add state to LoginModal

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,4 +1,13 @@
+import useLoginModal from "@/hooks/useLoginModal";
+import { useState } from "react";
+
 const LoginModal = () => {
+    const loginModal = useLoginModal();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
     return (
         <div></div>
     )
