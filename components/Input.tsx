1912a9bf73d915a1a3b9f0f034b00f5d69c24c83
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-08 19:05:55 +0200

    add Input component with props

---
+++ components/Input.tsx
@@ -0,0 +1,15 @@
+interface InputProps {
+    placeholder?: string;
+    value?: string;
+    type?: string;
+    disabled?: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default Input;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
-import Modal from '@/components/Modal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Modal actionLabel="Submit" isOpen title="Test Modal"/>
       <Layout>
         <Component {...pageProps} />
       </Layout>
