5282d411b55b2ef9b0257a06478c4f884418fb1d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-08 19:33:10 +0200

    add onSubmit func to LoginModal

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,5 +1,5 @@
 import useLoginModal from "@/hooks/useLoginModal";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 const LoginModal = () => {
     const loginModal = useLoginModal();
@@ -7,6 +7,18 @@ const LoginModal = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = useCallback(async () => {
+        try {
+            setIsLoading(true);
+
+            // TODO ADD LOG IN
+
+            loginModal.onClose();
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
 
     return (
         <div></div>
