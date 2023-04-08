e81e31f2afc23d4a7e195bb57c8797a32c0d5cce
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-08 19:23:26 +0200

    add LoginModal component

---
+++ components/modals/LoginModal.tsx
@@ -0,0 +1,7 @@
+const LoginModal = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default LoginModal;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import LoginModal from '@/components/modals/LoginModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
