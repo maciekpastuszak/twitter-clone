d69cf2122d68378e5717fd1a44f8dcf883f13570
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-15 13:31:11 +0200

    add debug and session strategy

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -42,4 +42,12 @@ export default NextAuth({
         }
       })
     ], 
-})
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+      strategy: 'jwt'
+    },
+    jwt: {
+      secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret: process.env.NEXTAUTH_SECRET
+});
