fcbde358694504ae775a1ff4b4370e1e1d449d09
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-15 13:28:02 +0200

    bugfix import

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import NextAuth from 'next-auth';
-import { CredentialsProvider } from 'next-auth/providers';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 import prisma from '@/libs/prismadb';
 
