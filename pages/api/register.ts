31e41db76e02139946c909dd84fd24fb604fb361
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-16 11:53:48 +0200

    create user with prisma in register

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import prisma from "@/libs/prismadb";
+
 export default async function handler(
     req: NextApiRequest, 
     res: NextApiResponse) {
@@ -9,9 +11,20 @@ export default async function handler(
         }
 
         try {
-            const { email. username, name, password} = req.body;
+            const { email, username, name, password} = req.body;
 
             const hashedPassword = await bcrypt.hash(password, 12);
+
+            const user = await prisma?.user.create({
+                data: {
+                    email,
+                    username,
+                    name,
+                    hashedPassword
+                }
+            });
+
+            return res.status(200).json(user);
         } catch (error) {
             console.log(error)
             return res.status(400).end()
