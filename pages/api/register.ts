303e9fd81941395953d1c2b5e68a2be9109d1d6e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-16 12:08:54 +0200

    bugfix nextApiResp

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -7,7 +7,7 @@ export default async function handler(
     req: NextApiRequest, 
     res: NextApiResponse) {
         if (req.method !== 'POST') {
-            return req.status(405).end();
+            return res.status(405).end();
         }
 
         try {
