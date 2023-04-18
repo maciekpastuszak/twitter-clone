0fd9b030db23084dc1df975ef1d9ccc8e03a40d9
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-18 12:04:45 +0200

    wrap the app with session proivder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '@/components/Layout'
-import LoginModal from '@/components/modals/LoginModal'
-import RegisterModal from '@/components/modals/RegisterModal'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '@/components/Layout';
+import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider>
     <RegisterModal />
     <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   )
 }
