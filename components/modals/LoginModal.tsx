c4b01cd5a7dcbe15405f19e46dc762f5e70543b0
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-19 11:53:02 +0200

    available sing in and show/hide link

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -3,6 +3,7 @@ import { useCallback, useState } from "react";
 import Input from "../Input";
 import Modal from "../Modal";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import { signIn } from "next-auth/react";
 
 const LoginModal = () => {
     const loginModal = useLoginModal();
@@ -24,7 +25,10 @@ const LoginModal = () => {
         try {
             setIsLoading(true);
 
-            // TODO ADD LOG IN
+            await signIn('credentials', {
+                email,
+                password
+            })
 
             loginModal.onClose();
         } catch (error) {
@@ -32,7 +36,7 @@ const LoginModal = () => {
         } finally {
             setIsLoading(false)
         }
-    }, []);
+    }, [loginModal, email, password]);
 
 const bodyContent = (
     <div className="flex flex-col gap-4">
