01c61baf8f75effb027ec53c370d88f99de8480b
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-20 09:51:16 +0200

    add logic to finding users in prisma

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prismadb";
 
 export default async function handler(
     req: NextApiRequest,
@@ -8,5 +9,16 @@ export default async function handler(
         return res.status(405).end();
     }
 
-    
+    try {
+        const users = await prisma.user.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+
+        return res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        return res.status(400).end();
+    }
 } 
