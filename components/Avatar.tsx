e08e04a85324b6dc276279c548ebd848c5c054be
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-21 14:22:08 +0200

    add useCallback on Avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,3 +1,6 @@
+import useUser from "@/hooks/useUser";
+import { useCallback } from "react";
+
 interface AvatarProps {
     userId: string;
     isLarge?: boolean;
@@ -7,7 +10,16 @@ interface AvatarProps {
 const Avatar: React.FC<AvatarProps> = ({ 
     userId, isLarge, hasBorder 
 }) => {
-  return <div></div>
+  const { data: fetchedUser} = useUser(userId);
+
+  const onClick = useCallback((event: any) => {
+    event.stopPropagation();
+  }, []);
+
+  
+  return (
+  <div></div>
+  );
 }
 
   export default Avatar;
