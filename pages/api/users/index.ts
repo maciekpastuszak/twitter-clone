f9cb79555caee69442a533891132d13e03f91ea2
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-21 14:16:23 +0200

    bugfixes - users

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,24 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/libs/prismadb";
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    if (req.method !== 'GET') {
-        return res.status(405).end();
-    }
+import prisma from '@/libs/prismadb';
 
-    try {
-        const users = await prisma.user.findMany({
-            orderBy: {
-                createdAt: 'desc'
-            }
-        });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
 
-        return res.status(200).json(users)
-    } catch (error) {
-        console.log(error);
-        return res.status(400).end();
-    }
-} 
+  try {
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+
+    return res.status(200).json(users);
+  } catch(error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
