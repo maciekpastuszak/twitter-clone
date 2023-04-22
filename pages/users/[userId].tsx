f9b58f2bc9cbf143d9e2b268e31c3c82b16b1442
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-22 22:49:28 +0200

    fetch user using useUser hook

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,6 +1,19 @@
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+import Header from "@/components/Header";
+import useUser from "@/hooks/useUser";
+
 const UserView = () => {
+    const router = useRouter();
+    const {userId} = router.query;
+
+    const { data: fetchedUser, isLoading } = useUser(userId as string)
+
     return (
-        <div></div>
+        <>
+        <Header showBackArrow label="User Profile"/>
+        </>
     );
 }
 
