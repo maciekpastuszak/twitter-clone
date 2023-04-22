3ad88080cfaf17aeee5e8b7cb99e9a8daf9b2b67
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-22 23:00:43 +0200

    add Avatar to USerHero

--- components/users/UserHero.tsx
+++ components/users/UserHero.tsx
@@ -17,6 +17,9 @@ const UserHero: React.FC<UserHeroProps> = ({ userId }) => {
         {fetchedUser?.coverImage && (
           <Image src={fetchedUser.coverImage} fill alt="Cover Image" style={{ objectFit: 'cover' }}/>
         )}
+        <div className="absolute bottom-16 left-4">
+          <Avatar userId={userId} isLarge hasBorder />
+        </div>
       </div>
     </div>
    );
