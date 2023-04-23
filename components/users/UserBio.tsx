0150c5a06cf3623934505372837b8cf7f03da018
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-23 22:32:32 +0200

    add edit/follow button to userBio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,8 +1,9 @@
 import { format } from "date-fns";
+import { useMemo } from "react";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
-import { useMemo } from "react";
+import Button from "../Button";
 
 interface UserBioProps {
     userId: string;
@@ -21,8 +22,19 @@ const UserBio: React.FC<UserBioProps> = ( { userId } ) => {
         return format(new Date(fetchedUser.createAt), 'MMMM yyyy');
     }, [fetchedUser?.createAt]);
     return (
-        <div></div>
-    )
+        <div className="border-b-[1px] border-neutral-800 pb-4">
+            <div className="flex justify-end p-2">
+                {currentUser?.id === userId ? (
+                    <Button secondary label="Edit" onClick={() => {}}/>
+                ) : (
+                    <Button 
+                    onClick={() => {}}
+                    label="Follow"
+                    secondary />
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default UserBio;
