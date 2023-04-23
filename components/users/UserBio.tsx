005609e8f3110e91cf3d3b2db97821428fe75e7b
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-23 22:21:29 +0200

    add fetch user to user bio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,9 +1,14 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useUser from "@/hooks/useUser";
+
 interface UserBioProps {
     userId: string;
 }
 
 
 const UserBio: React.FC<UserBioProps> = ( { userId } ) => {
+    const { data:currentUser } = useCurrentUser();
+    const { data: fetchUser } = useUser(userId);
     return (
         <div></div>
     )
