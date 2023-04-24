d2a90b5e1c1c3fda801ed57d150215eed0879172
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-24 09:51:00 +0200

    add EditModal component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import LoginModal from '@/components/modals/LoginModal';
 import RegisterModal from '@/components/modals/RegisterModal';
+import EditModal from '@/components/modals/EditModal';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
     <RegisterModal />
     <LoginModal />
       <Layout>
