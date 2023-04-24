29ac225bf91443e46ada3f5d398afe668e38ee6d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-24 09:48:10 +0200

    add editModal to userBio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -5,6 +5,7 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 import Button from "../Button";
 import { BiCalendar } from "react-icons/bi";
+import useEditModal from "@/hooks/useEditModal";
 
 interface UserBioProps {
     userId: string;
@@ -14,6 +15,8 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ( { userId } ) => {
     const { data: currentUser } = useCurrentUser();
     const { data: fetchedUser } = useUser(userId);
+
+    const editModal = useEditModal();
 
     const createdAt = useMemo(() => {
         if(!fetchedUser?.createdAt) {
@@ -26,7 +29,7 @@ const UserBio: React.FC<UserBioProps> = ( { userId } ) => {
         <div className="border-b-[1px] border-neutral-800 pb-4">
             <div className="flex justify-end p-2">
                 {currentUser?.id === userId ? (
-                    <Button secondary label="Edit" onClick={() => {}}/>
+                    <Button secondary label="Edit" onClick={editModal.onOpen}/>
                 ) : (
                     <Button 
                     onClick={() => {}}
