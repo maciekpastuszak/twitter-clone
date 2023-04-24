01d8665f421560695f2eddaa9eba42821a5df9e0
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-24 09:23:46 +0200

    update user using prisma

--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -12,11 +12,26 @@ export default async function handler(
     try {
         const { currentUser } = await serverAuth(req,res);
 
-        const { name, username, bio, profilImage, converImage } = req.body;
+        const { name, username, bio, profileImage, coverImage } = req.body;
         
         if (!name || username) {
             throw new Error('Missing fields');
         }
+
+        const updateUser = await prisma?.user.update({
+            where: {
+                id: currentUser.id
+            },
+            data: {
+                name,
+                username,
+                bio,
+                profileImage,
+                coverImage
+            }
+        });
+
+        return res.status(200).json(updateUser);
     
     } catch (error) {
         console.log(error)
