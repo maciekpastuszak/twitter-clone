e0126c8daebdccd54856da1928e8185233802a9c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-25 08:57:37 +0200

    add props to EditModal

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useEditModal from "@/hooks/useEditModal";
+import useUser from "@/hooks/useUser";
+
+
 const EditModal = () => {
+    const { data: currentUser } = useCurrentUser();
+    const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
+    const editModal = useEditModal();
+
+    const [profilImage, setProfilImage] = useState();
+    
     return (
         <div></div>
     )
