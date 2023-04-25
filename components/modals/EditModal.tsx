daf5319c7b3e45287b22e12918dd1eac3e135f4c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-25 10:11:21 +0200

    add imageUpload to EditModal

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -8,6 +8,7 @@ import { toast } from "react-hot-toast";
 import axios from "axios";
 import Modal from "../Modal";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 const EditModal = () => {
     const { data: currentUser } = useCurrentUser();
@@ -55,6 +56,18 @@ const EditModal = () => {
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
+            <ImageUpload 
+                value={profileImage}
+                disabled={isLoading}
+                onChange={(image) => setProfileImage(image)}
+                label="Upload profile image"
+            />
+            <ImageUpload 
+                value={coverImage}
+                disabled={isLoading}
+                onChange={(image) => setCoverImage(image)}
+                label="Upload cover image"
+            />
             <Input 
                 placeholder="Name"
                 onChange={(e) => setName(e.target.value)}
