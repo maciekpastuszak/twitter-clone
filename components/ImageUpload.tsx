14d00359bf7157289d6467b1c3b8e59bf2e64955
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-26 08:56:34 +0200

    add image upload props

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,4 +1,16 @@
-const ImageUpload = () => {
+interface ImageUploadProps {
+  onChange: (base64: string) => void;
+  label: string;
+  value?: string;
+  disabled?: boolean;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+    onChange,
+    label,
+    value,
+    disabled
+}) => {
     return (
         <div></div>
     )
