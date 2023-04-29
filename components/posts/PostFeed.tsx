642f27edab88af337dcb649b368da3782ae3131e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-29 10:59:46 +0200

    iterate through posts in PostFeed

--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -1,10 +1,22 @@
+import usePosts from "@/hooks/usePosts";
+
 interface PostFeedProps {
     userId?: string
 }
 
 const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+    const { data: posts = [] } = usePosts(userId);
+    
     return(
-        <div></div>
+        <>
+        {posts.map((post: Record<string, any>) => (
+            <PostItem
+                userId={userId}
+                key={post.id}
+                data={post}
+            />
+        ))}
+        </>
     )
 }
 
