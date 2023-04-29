7fcc1e3b00f241093dc6a84ff8323da705447efc
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-29 11:12:23 +0200

    add hooks to PostItem component

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,9 +1,25 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useLoginModal from "@/hooks/useLoginModal";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
 interface PostItemProps {
     data: Record<string, any>;
     userId?:string;
 }
 
 const PostItem: React.FC<PostItemProps> = ({data, userId}) => {
+    const router = useRouter();
+    const loginModal = useLoginModal();
+
+    const { data:currentUser } = useCurrentUser()
+
+    const goToUser = useCallback((event: any) => {
+        event.stopPropagation();
+
+        router.push(`/users/${data.user.id}`);
+    }, [router, data.user.id])
+    
     return (
         <div>
 
