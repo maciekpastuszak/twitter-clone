bcb49eaf5568879e71c673d90c9ce39ca860a5b8
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-30 11:39:18 +0200

    bugfix register helper

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,32 +1,30 @@
 import bcrypt from 'bcrypt';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
-import prisma from "@/libs/prismadb";
+import prisma from '@/libs/prismadb';
 
-export default async function handler(
-    req: NextApiRequest, 
-    res: NextApiResponse) {
-        if (req.method !== 'POST') {
-            return res.status(405).end();
-        }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
 
-        try {
-            const { email, username, name, password} = req.body;
+  try {
+    const { email, username, name, password } = req.body;
 
-            const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-            const user = await prisma?.user.create({
-                data: {
-                    email,
-                    username,
-                    name,
-                    hashedPassword
-                }
-            });
+    const user = await prisma.user.create({
+      data: {
+        email,
+        username,
+        name,
+        hashedPassword,
+      }
+    });
 
-            return res.status(200).json(user);
-        } catch (error) {
-            console.log(error)
-            return res.status(400).end()
-        }
-    }
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
