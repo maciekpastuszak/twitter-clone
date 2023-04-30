7b06d1db123a2d25753514249998a55b19a756a7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-30 10:37:09 +0200

    add comment to post

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -4,6 +4,7 @@ import { formatDistanceToNowStrict } from "date-fns";
 import { useRouter } from "next/router";
 import { useCallback, useMemo } from "react";
 import Avatar from "../Avatar";
+import { AiOutlineMessage } from "react-icons/ai";
 
 interface PostItemProps {
     data: Record<string, any>;
@@ -70,6 +71,31 @@ const PostItem: React.FC<PostItemProps> = ({data, userId}) => {
                         <span className="text-neutral-500 text-sm">
                             {createdAt}
                         </span>
+                    </div>
+                    <div className="text-white mt-1">
+                        {data.body}
+                    </div>
+                    <div className="
+                        flex 
+                        flex-row 
+                        items-center 
+                        mt-3 
+                        gap-10">
+                        <div className="
+                            flex
+                            flex-row
+                            items-center
+                            text-neutral-500
+                            gap-2
+                            cursor-pointer
+                            transition
+                            hover:text-sky-500
+                        ">
+                            <AiOutlineMessage size={20}/>
+                            <p>
+                                {data.comments?.length || 0}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
