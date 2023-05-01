f00f37b44ec4adbcfbea8dac9580753dd0c84f80
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-01 10:30:47 +0200

    add follow handler

---
+++ pages/api/follow.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== 'POST' && req.method !== 'DELETE') {
+        return res.status(405).end();
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(400).end()
+    }
+}
