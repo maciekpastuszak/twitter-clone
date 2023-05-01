65cff41a729db91fe63f6b62ff5143340a616746
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-01 11:01:14 +0200

    add props to useFollow

--- hooks/useFollow.ts
+++ hooks/useFollow.ts
@@ -1,7 +1,23 @@
+import { useCallback, useMemo } from "react";
 import useCurrentUser from "./useCurrentUser"
+import useLoginModal from "./useLoginModal";
 import useUser from "./useUser";
 
 const useFollow = (userId: string) => {
     const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
-    const { mutate: mutateFetchedUser } = useUser(userId)
+    const { mutate: mutateFetchedUser } = useUser(userId);
+
+    const loginModal = useLoginModal();
+
+    const isFollowing = useMemo(() => {
+        const list = currentUser?.followingIds || [];
+    
+        return list.includes(userId);
+      }, [currentUser, userId]);
+    
+      const toggleFollow = useCallback(async () => {
+        if (!currentUser) {
+          return loginModal.onOpen();
+        }
+    }
 }
