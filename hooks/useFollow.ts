953a4ad0207a54ef1fcfac563a87b431f365d7e7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-02 10:24:48 +0200

    add axios paths

--- hooks/useFollow.ts
+++ hooks/useFollow.ts
@@ -1,9 +1,11 @@
+import axios from "axios";
 import { useCallback, useMemo } from "react";
 import { toast } from "react-hot-toast";
 
 import useCurrentUser from "./useCurrentUser"
 import useLoginModal from "./useLoginModal";
 import useUser from "./useUser";
+
 
 
 const useFollow = (userId: string) => {
@@ -24,7 +26,13 @@ const useFollow = (userId: string) => {
         }
         try {
             let request;
-            
+
+            if (isFollowing) {
+                request = () => axios.delete('/api/follow', {data: { userId } });
+            } else {
+                request = () => axios.post('/api/follow', { userId });
+            }
+
         } catch (error) {
             toast.error('Something went wrong')
         }
