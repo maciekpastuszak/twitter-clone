2668efcab0f9ec75cc4d4f7bc4b30dc8fe84715d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-02 10:36:11 +0200

    add toggleFollow to a btn in UserBio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -6,6 +6,7 @@ import useUser from "@/hooks/useUser";
 import Button from "../Button";
 import { BiCalendar } from "react-icons/bi";
 import useEditModal from "@/hooks/useEditModal";
+import useFollow from "@/hooks/useFollow";
 
 interface UserBioProps {
     userId: string;
@@ -17,6 +18,8 @@ const UserBio: React.FC<UserBioProps> = ( { userId } ) => {
     const { data: fetchedUser } = useUser(userId);
 
     const editModal = useEditModal();
+
+    const { isFollowing, toggleFollow } = useFollow(userId);
 
     const createdAt = useMemo(() => {
         if(!fetchedUser?.createdAt) {
@@ -32,8 +35,8 @@ const UserBio: React.FC<UserBioProps> = ( { userId } ) => {
                     <Button secondary label="Edit" onClick={editModal.onOpen}/>
                 ) : (
                     <Button 
-                    onClick={() => {}}
-                    label="Follow"
+                    onClick={toggleFollow}
+                    label={isFollowing ? 'Unfollow' : 'Follow'}
                     secondary />
                 )}
             </div>
