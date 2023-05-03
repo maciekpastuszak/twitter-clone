c4de79a5b6a1316a1a623468c427f6720025d126
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-03 22:09:56 +0200

    add basic logic to liking handler

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -3,4 +3,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
-)
+) {
+    if (req.method !== 'POST' && req.method !== 'DELETE') {
+        return res.status(405).end();
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(400).end();
+    }
+}
