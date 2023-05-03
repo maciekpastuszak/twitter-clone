15ba100811013791f201b9a84d75dd1645c4f0b8
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-03 22:03:15 +0200

    add loader to post view

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,4 +1,20 @@
+import usePost from "@/hooks/usePost";
+import { ClipLoader } from "react-spinners";
+import { useRouter } from "next/router";
+
 const PostView = () => {
+    const router = useRouter();
+    const { postId } = router.query;
+
+    const { data: fetchedPost, isLoading } = usePost(postId as string);
+
+    if (isLoading || !fetchedPost) {
+        return (
+            <div className="flex justofy-center items-center h-full">
+                <ClipLoader color="lightblue" size={80}/>
+            </div>
+        )
+    }
     return (
         <div>
 
