5415074d09c346a969da19799990e6d174c49896
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-05 10:32:42 +0200

    protect the route and extract current user

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,4 +1,6 @@
+import serverAuth from "@/libs/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prismadb";
 
 export default async function handler(
     req: NextApiRequest,
@@ -9,6 +11,23 @@ export default async function handler(
     }
 
     try {
+        const { currentUser } = await serverAuth(req,res);
+        const { body } = req.body;
+        const { postId } = req.query;
+
+        if (!postId || typeof postId !== 'string') {
+            throw new Error("Invalid ID");
+        }
+
+        const comment = await prisma.comment.create({
+            data: {
+                body,
+                userId: currentUser.id,
+                postId
+            }
+        });
+
+        return res.status(200).json(comment);
         
     } catch (error) {
         console.log(error)
