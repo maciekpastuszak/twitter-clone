892070555ad5c91a8a4d41bfdcc2be4c2ae1a406
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-05 10:27:56 +0200

    add main logic to comments

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -4,5 +4,14 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(400).end();
+    }
 }
