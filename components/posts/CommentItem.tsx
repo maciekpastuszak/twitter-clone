0f82a2066d6bc75af3535fe9a63fa63e64526421
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-06 10:12:37 +0200

    add hooks to CommentItems

--- components/posts/CommentItem.tsx
+++ components/posts/CommentItem.tsx
@@ -1,8 +1,28 @@
+import { formatDistanceToNowStrict } from "date-fns";
+import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+
 interface CommentItemProps {
     data: Record<string, any>
 }
 
 const CommentItem: React.FC<CommentItemProps> = ({ data }) => {
+const router = useRouter();
+
+const goToUser = useCallback((event: any) => {
+    event.stopPropagation();
+
+    router.push(`/users/${data.user.id}`);
+}, [router, data.user.id]);
+
+const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+        return null
+    }
+
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+}, [data?.createdAt])
+
     return (
         <div></div>
     )
