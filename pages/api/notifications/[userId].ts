80d86784ef6ac1d465ae654c7fafd248aa428ce5
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-07 21:36:54 +0200

    fetch userid from query in notifications

--- pages/api/notifications/[userId].ts
+++ pages/api/notifications/[userId].ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -6,5 +7,18 @@ export default async function handler(
     ) {
     if (req.method !== 'GET') {
         return res.status(405).end()
+    }
+
+    try {
+        const { userId } = req.query;
+
+        if(!userId || typeof userId !== 'string') {
+            throw new Error('Invallid ID');
+        }
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).end();
     }
 }
