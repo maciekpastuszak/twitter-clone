0fbf781de5943422238cef254aa9dd2a863e1394
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-07 21:39:47 +0200

    retrive notifications from db

--- pages/api/notifications/[userId].ts
+++ pages/api/notifications/[userId].ts
@@ -1,6 +1,8 @@
 import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 
+import prisma from "@/libs/prismadb";
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
@@ -16,7 +18,15 @@ export default async function handler(
             throw new Error('Invallid ID');
         }
 
-
+        const notifications = await prisma.notification.findMany({
+            where: {
+                userId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        
     } catch (error) {
         console.log(error)
         return res.status(400).end();
