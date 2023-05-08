fed5cf60b0b1526dded6da9a66d69d4c4abeaeae
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-08 10:50:11 +0200

    add props to NotificationsFeed

--- components/ NotificationsFeed.tsx
+++ components/ NotificationsFeed.tsx
@@ -1,4 +1,15 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useNotifications from "@/hooks/useNotifications";
+import { useEffect } from "react";
+
 const NotificationsFeed = () => {
+const { data: currentUser , mutate: mutateCurrentUser } = useCurrentUser();
+const { data: fetchedNotifications = [] } = useNotifications(currentUser?.id);
+
+useEffect(() => {
+    mutateCurrentUser();
+}, [mutateCurrentUser]);
+
     return (
         <div></div>
     );
