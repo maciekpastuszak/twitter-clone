bcd61408554e57af2674d25ab8dfb0aa45cec8fa
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-08 10:54:45 +0200

    add twitter icon to notifications

--- components/ NotificationsFeed.tsx
+++ components/ NotificationsFeed.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useNotifications from "@/hooks/useNotifications";
 import { useEffect } from "react";
+import { BsTwitter } from "react-icons/bs";
 
 const NotificationsFeed = () => {
 const { data: currentUser , mutate: mutateCurrentUser } = useCurrentUser();
@@ -21,7 +22,12 @@ if (fetchedNotifications.length === 0) {
     return (
         <div className="flex flex-col">
             {fetchedNotifications.map((notification: Record<string, any>) => (
-        
+            <div key={notification.id} className="flex flex-row items-center p-6 gap-4 border-b-[1px] border-neutral-800">
+                <BsTwitter color="white" size={32} />
+                <p>
+                    {notification.body}
+                </p>
+            </div>
         ))}
         </div>
     );
