efddfd930a0f5723deb32ff0ff9c173d2fba242a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-08 10:32:30 +0200

    add useNotifications hook

--- pages/api/notifications/[userId].ts
+++ pages/api/notifications/[userId].ts
@@ -36,7 +36,7 @@ export default async function handler(
             }
           });
 
-        return res.status(200).json(notifications)
+        return res.status(200).json(notifications);
     } catch (error) {
         console.log(error)
         return res.status(400).end();
